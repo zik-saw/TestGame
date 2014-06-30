5fa54c19d34274cf1a7e8bd014ed4f306f4d3033
helloween141 <helloween141@gmail.com>
2014-06-30 17:14:54 +0600

    Update WinMain.cpp

--- WinMain.cpp
+++ WinMain.cpp
@@ -4,18 +4,19 @@
 bool GenerateWindow(HINSTANCE hInstance, int nCmdShow, LPCTSTR className, LPCTSTR windowTitle, int width, int height);
 bool GenerateWindow(HINSTANCE hInstance, int nCmdShow, LPCTSTR className, LPCTSTR windowTitle, int x, int y ,int width, int height);
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wPARAM, LPARAM lParam);
 
 //основная функция
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInsce, LPSTR lpCmdLine, int nCmdShow)
-
-	if(GenerateWindow(hInstance, nCmdShow, L"Win32ow", L"Your first Win32 Window", 1280, 720))
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if(GenerateWindow(hInstance, nCmdShow, L"Win32Window", L"Your first Win32 Window", 1280, 720))
 	{
 		MSG msg;
 
 		while(true)
 		{
 			while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) //проверяет есть ли в очереди сообщения и программа работает дальше(в отличиии от getMessage, которая ожидает пока не выолнися текущее событие(в духе синхронных и асинхронных процессов))
-			{//PM_REMOVE - Удалять сообщения из оеди
+			{//PM_REMOVE - Удалять сообщения из очереди
 				TranslateMessage(&msg); //преобразование клавиатурного ввода
 
 				DispatchMessage(&msg);// обработка и пересылка сообщений в WindowPr
@@ -36,7 +37,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInsce, LPSTR lpCmdLine, i
 
 bool GenerateWindow(HINSTANCE hInstance, int nCmdShow, LPCTSTR className, LPCTSTR windowTitle, int width, int height)
 {
-	return GenerateWindow(hInstance, nCmdShow, csName, windowTitle, (GetSystemMetrics(SM_CXSCREEN) - width) / 2, 
+	return GenerateWindow(hInstance, nCmdShow, className, windowTitle, (GetSystemMetrics(SM_CXSCREEN) - width) / 2, 
 		(GetSystemMetrics(SM_CXSCREEN) - height)/2 , width, height);
 }
 
@@ -49,12 +50,12 @@ bool GenerateWindow(HINSTANCE hInstance, int nCmdShow, LPCTSTR className , LPCTS
 
 	ZeroMemory(&wcex, sizeof(WNDCLASSEX));
 	wcex.cbSize = sizeof(wcex);
-	wcex.style = CS_HREDRAW | CSEDRAW;
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc = WindowProc;
 	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIconL, IDI_APPLICATION);
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
 	wcex.lpszClassName = className;
 	wcex.hIcon = LoadIcon(NULL, IDI_WINLOGO);
 
@@ -64,12 +65,12 @@ bool GenerateWindow(HINSTANCE hInstance, int nCmdShow, LPCTSTR className , LPCTS
 	}
 
 
-	hWnd = CreateWindowEx(NULL, ssName, windowTitle, WS_VISIBLE, 0,0, width, height, NULL,NULL,hInstance, NULL);
+	hWnd = CreateWindowEx(NULL, className, windowTitle, WS_VISIBLE, 0,0, width, height, NULL,NULL,hInstance, NULL);
 
 	//мутит полноэкранное окно
 	//hWnd = CreateWindowEx(WS_EX_WINDOWEDGE , className, windowTitle, WS_VISIBLE, 0,0, GetSystemMetrics(SM_CXSCREEN),GetSystemMetrics(SM_CXSCREEN), NULL,NULL,hInstance, NULL);
 	//SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & (~WS_CAPTION));
-	
+
 	ShowWindow(hWnd, nCmdShow);
 
 	return true;
@@ -77,19 +78,22 @@ bool GenerateWindow(HINSTANCE hInstance, int nCmdShow, LPCTSTR className , LPCTS
 
 
 //обработчик события окна
-LRESULT CALLBACK WindohWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch(message)
 	{
 	case WM_DESTROY:
 		{
-			PostQuige(0); //ставит в очередь сообщение WM_destroy
+			PostQuitMessage(0); //ставит в очередь сообщение WM_destroy
 
 			return 0;
 		}break;
 
 	case WM_PAINT:
-	
+		break;
+
 	}
 
+	return DefWindowProc(hWnd, message, wParam, lParam); //обрабатывает те сообщения,которые вы не используете
 
+}
